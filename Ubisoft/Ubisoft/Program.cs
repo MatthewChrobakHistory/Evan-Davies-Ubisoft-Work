5bd7c383597db50c2c0c6577ab6de49845537659
History <leftistlucy@hotmail.com>
2017-09-02 20:52:06 -0400

    First Commit

---
+++ Ubisoft/Ubisoft/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ubisoft
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+
+            int val = 60 * 30 * 1000, tick = 0;
+            int tmrscrc = 0;
+            val += Environment.TickCount;
+            var sc = new ScreenCapture();
+
+            while (true) {
+
+                tick = Environment.TickCount;
+
+                if (tmrscrc < tick) {
+
+                    
+                    var img = sc.CaptureScreen();
+                    img.Save(path + (val - tick) + ".png");
+
+
+                    tmrscrc = 7000 + tick;
+                }
+            }
+        }
+    }
+}
---
+++ UbisoftSplitter/UbisoftSplitter/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Linq;
+
+namespace UbisoftSplitter
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            string folder = path + "\\input\\";
+            string output = path + "\\output\\";
+            const int startY = 145;
+            const int startX = 48;
+            const int finishX = 566;
+            const int finishY = startY + 360;
+            int count = 0;
+
+
+            foreach (var file in Directory.GetFiles(folder)) {
+                var fi = new FileInfo(file);
+                var bmp = new Bitmap(Image.FromFile(file));
+                var obmp = new Bitmap(finishX - startX, finishY - startY);
+
+                for (int x = startX; x < finishX; x++) {
+                    for (int y = startY; y < finishY; y++) {
+                        var color = bmp.GetPixel(x, y);
+                        obmp.SetPixel(x - startX, y - startY, color);
+                    }
+                }
+
+                obmp.Save(output + fi.Name + ".png");
+            }
+        }
+    }
+}
